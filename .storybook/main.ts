0268e818c3aa5129583d4e0fb7d9f67b995d3330
João Vitor <jvmnc232@gmail.com>
2024-09-19 23:15:51 -0300

    chore: format project with prettier rules

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,17 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
 };
 export default config;
