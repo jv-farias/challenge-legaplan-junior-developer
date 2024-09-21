bcbd18b86a0447a4e2a3b184fa14ecbf85eafe88
João Vitor <jvmnc232@gmail.com>
2024-09-21 14:49:19 -0300

    chore: update plopfile template configuration

--- generators/plopfile.mjs
+++ generators/plopfile.mjs
@@ -1,37 +1,40 @@
 const plopConfig = (/** @type {import('plop').NodePlopAPI} */ plop) => {
-    plop.setGenerator('component', {
-      description: 'Create a component',
-      prompts: [
-        {
-          type: 'input',
-          name: 'name',
-          message: 'What is your component name?',
-        },
-      ],
-      actions: [
-        {
-          type: 'add',
-          path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.index.tsx',
-          templateFile: 'templates/Component.tsx.hbs',
-        },
-        {
-          type: 'add',
-          path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
-          templateFile: 'templates/stories.tsx.hbs',
-        },
-        {
-          type: 'add',
-          path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
-          templateFile: 'templates/test.tsx.hbs',
-        },
-        {
-          type: 'add',
-          path: '../src/components/{{pascalCase name}}/index.ts',
-          templateFile: 'templates/index.ts.hbs',
-        },
-      ],
-    });
-  };
-  
-  export default plopConfig;
-  
+  plop.setGenerator("component", {
+    description: "Create a component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is your component name?",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
+        templateFile: "templates/Component.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx",
+        templateFile: "templates/stories.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx",
+        templateFile: "templates/test.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/index.ts",
+        templateFile: "templates/index.ts.hbs",
+      },
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.module.scss",
+      },
+    ],
+  });
+};
+
+export default plopConfig;
